022c7ece19ce4aaad05efa3198c4843a94ecfad7
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-12-01 17:54:37 -0300

    fix: Fix mainsite/urls and views

--- web/political_advisor_app/views.py
+++ web/political_advisor_app/views.py
@@ -7,4 +7,5 @@ redis = Redis(host='redis', port=6379)
 # Create your views here.
 def home(request):
     counter = redis.incr('counter')
-    return render(request, 'home.html', {'counter': counter})
+    return render(request, 'political_advisor_app/home.html',
+                  {'counter': counter})
