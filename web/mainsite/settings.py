46996a7902d1e414b9e0362c81febdda8588bc4e
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-12-01 21:11:15 -0300

    refactor: Rename political_advisor_app to myapp

--- web/mainsite/settings.py
+++ web/mainsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # apps
-    'political_advisor_app',
+    'myapp',
 ]
 
 MIDDLEWARE = [
--- web/myapp/views.py
+++ web/myapp/views.py
@@ -7,5 +7,5 @@ redis = Redis(host='redis', port=6379)
 # Create your views here.
 def home(request):
     counter = redis.incr('counter')
-    return render(request, 'political_advisor_app/home.html',
+    return render(request, 'myapp/home.html',
                   {'counter': counter})
