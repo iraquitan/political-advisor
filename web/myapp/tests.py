41d812f135c9faf42eccfb9eb4b4a402dc253d1f
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-12-07 01:40:05 -0300

    fix: Fix env variables and test

--- web/myapp/tests.py
+++ web/myapp/tests.py
@@ -4,4 +4,4 @@ from django.test import TestCase
 # Create your tests here.
 class BaseTests(TestCase):
     def test_base(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
