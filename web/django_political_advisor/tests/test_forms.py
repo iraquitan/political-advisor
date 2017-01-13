459b40fc7fd77ea51b9c434b27cb17f6324f1b86
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2017-01-13 09:52:37 -0300

    test: Add initial tests to AssessorProfileForm

--- web/django_political_advisor/tests/test_forms.py
+++ web/django_political_advisor/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from ..assessor.forms import AssessorForm
+from ..assessor.forms import AssessorForm, AssessorProfileForm
 
 
 class AssessorFormTest(TestCase):
@@ -17,4 +17,28 @@ class AssessorFormTest(TestCase):
 
     def test_blank_data(self):
         form = AssessorForm({})
+        self.assertFalse(form.is_valid())
+
+
+class AssessorProfileFormTest(TestCase):
+    def setUp(self):
+        self.form_valid_data = {
+            'gender': 'M',
+            'picture': None,
+        }
+        self.form_invalid_data = {
+            'gender': 'T',
+            'picture': None,
+        }
+
+    def test_valid_data(self):
+        form = AssessorProfileForm(self.form_valid_data)
+        self.assertTrue(form.is_valid())
+
+    def test_blank_data(self):
+        form = AssessorProfileForm({})
+        self.assertFalse(form.is_valid())
+
+    def test_invalid_data(self):
+        form = AssessorProfileForm(self.form_invalid_data)
         self.assertFalse(form.is_valid())
